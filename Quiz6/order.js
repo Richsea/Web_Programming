1515b579c13c1207dd3b797c41f46c41aa0015a6
Richsea <bhyeh1@gmail.com>
2019-11-22 15:55:37 +0900

    start

---
+++ Quiz6/order.js
@@ -0,0 +1,20 @@
+let infoArray = new Array();
+
+function getData()
+{
+    let title_str = $("#title").val();
+    let price_str = $("#price").val();
+
+    console.log(title_str);
+    console.log(price);
+}
+
+window.onload = function()
+{
+    let dataList = $("#bb tr td");
+
+    for(let i = 0; i < dataList.length; i++)
+    {
+        
+    }
+}
