230991de77d12213660ed6911e5bedac05b9b293
Richsea <bhyeh1@gmail.com>
2019-11-30 02:53:00 +0900

    finish quiz7

--- Quiz7/Quiz7.js
+++ Quiz7/Quiz7.js
@@ -2,16 +2,15 @@ let c = document.getElementById('draw');
 let v = document.getElementById('video1');
 let ctx = c.getContext("2d");
 let i;
+
 v.addEventListener("play",
                    function() {
-                      i = window.setInterval(function()       
+                      i = window.setInterval(function()
                         {ctx.drawImage(v,5,5,390,390)}
                         ,33);
                    }, false);
-v.addEventListener("pause", function() {window.clearInterval(i);}, false);
 v.addEventListener("ended", function() {clearInterval(i);}, false); 
 
-// ctx.save();
 ctx.setTransform(1,0,0,1,0,0);
 
 let videoWidth = v.clientWidth;
@@ -19,15 +18,19 @@ let videoHeight = v.clientHeight;
 
 let rotation = 30;
 
-let angleInRadians=  rotation * Math.PI/180;
+function rotate()
+{
+  ctx.clearRect(0, 0, c.width, c.height);
+  let angleInRadians = rotation * Math.PI/180;
+  let x = 0, y = 0;
 
-let x = 0, y =0;
+  ctx.translate(x+.5*videoWidth, y+.5*videoHeight);
+  ctx.rotate(angleInRadians);
 
-ctx.translate(x+.5*videoWidth, y+.5*videoHeight);
-console.log(x+.5*videoWidth, y+.5*videoHeight, angleInRadians);
-ctx.rotate(angleInRadians);
+  ctx.restore();
+  rotation ++;
+  
+}
 
-// ctx.restore( )
-// rotation ++;
 
 
