3f3e4e2bb907e3d4a0c5754db55d93042685c395
Richsea <bhyeh1@gmail.com>
2019-12-06 20:22:15 +0900

    Quiz8 set table row column. bug: Quiz7 frame didn't work well

--- Quiz7/Quiz7.js
+++ Quiz7/Quiz7.js
@@ -2,21 +2,25 @@ let c = document.getElementById('draw');
 let v = document.getElementById('video1');
 let ctx = c.getContext("2d");
 let i;
+let rotateStart = false;
 
 v.addEventListener("play",
                    function() {
                       i = window.setInterval(function()
                         {ctx.drawImage(v,5,5,390,390)}
                         ,33);
-                   }, false);
-v.addEventListener("ended", function() {clearInterval(i);}, false); 
+                   });
 
+v.addEventListener("ended", function() {clearInterval(i);}, false);
+
+ctx.save();
 ctx.setTransform(1,0,0,1,0,0);
 
 let videoWidth = v.clientWidth;
 let videoHeight = v.clientHeight;
 
 let rotation = 30;
+
 
 function rotate()
 {
@@ -28,9 +32,11 @@ function rotate()
   ctx.rotate(angleInRadians);
 
   ctx.restore();
-  rotation ++;
-  
+  // rotation ++;
 }
 
-
-
+function tryClick()
+{
+  rotateStart = true;
+  // rotate();
+}
